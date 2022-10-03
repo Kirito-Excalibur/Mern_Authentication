c3c41d37675c7b696a4ec4e58f4a0ff3ac852869
Kirito <nikhilsimontoppo@gmail.com>
2022-10-03 22:21:10 +0530

    back_to_pc

--- frontend/src/components/Signup.js
+++ frontend/src/components/Signup.js
@@ -1,9 +1,25 @@
 import { Button, TextField, Typography } from "@mui/material";
 import { Box } from "@mui/system";
-import React from "react";
+import React, { useState } from "react";
 
 const Signup = () => {
-  const handleSumbit = () => {};
+  const [inputs, setinputs] = useState({
+    name:"",
+    email:"",
+    password:""
+  }
+  )
+  const handleChange=(e)=>{
+    setinputs((prev)=>({
+      ...prev,
+      [e.target.name]:e.target.value,
+    }))
+  }
+  const handleSumbit = (e) => {
+    e.preventDefault()
+    console.log(inputs)
+  };
+
   return (
     <div>
       <form onSubmit={handleSumbit}>
@@ -17,9 +33,9 @@ const Signup = () => {
           alignItems="center"
         >
           <Typography variant="h2">SignUp</Typography>
-          <TextField variant="outlined" placeholder="Name" margin="normal" />
-          <TextField variant="outlined" placeholder="Email" margin="normal" />
-          <TextField variant="outlined" placeholder="Password" margin="normal" />
+          <TextField name="name"  onChange={handleChange} value={inputs.name} variant="outlined" placeholder="Name" margin="normal" />
+          <TextField  name="email" onChange={handleChange} type={"email"} value={inputs.email} variant="outlined" placeholder="Email" margin="normal" />
+          <TextField  name="password" onChange={handleChange} type={"password"} value={inputs.password} variant="outlined" placeholder="Password" margin="normal" />
           <Button variant="contained" type="sumbit">Signup</Button>
         </Box>
       </form>
