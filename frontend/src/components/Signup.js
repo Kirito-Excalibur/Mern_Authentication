29f225e41553356420cc9bf7539c523ce399edf7
Nikhil <nikhilsimontoppo@gmail.com>
2022-10-03 20:31:05 +0530

    switch

--- frontend/src/components/Signup.js
+++ frontend/src/components/Signup.js
@@ -1,9 +1,30 @@
-import React from 'react'
+import { Button, TextField, Typography } from "@mui/material";
+import { Box } from "@mui/system";
+import React from "react";
 
 const Signup = () => {
+  const handleSumbit = () => {};
   return (
-    <div>Signup</div>
-  )
-}
+    <div>
+      <form onSubmit={handleSumbit}>
+        <Box
+          marginLeft="auto"
+          marginRight="auto"
+          display="flex"
+          flexDirection="column"
+          width="300px"
+          justifyContent="center"
+          alignItems="center"
+        >
+          <Typography variant="h2">SignUp</Typography>
+          <TextField variant="outlined" placeholder="Name" margin="normal" />
+          <TextField variant="outlined" placeholder="Email" margin="normal" />
+          <TextField variant="outlined" placeholder="Password" margin="normal" />
+          <Button variant="contained" type="sumbit">Signup</Button>
+        </Box>
+      </form>
+    </div>
+  );
+};
 
-export default Signup
+export default Signup;
