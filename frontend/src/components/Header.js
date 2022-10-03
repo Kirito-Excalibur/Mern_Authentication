21c5c22c4491c4da36c3d6d926c6618f83d80aeb
Nikhil <nikhilsimontoppo@gmail.com>
2022-10-03 23:11:33 +0530

    frontend-connectedTo-backend

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,16 +1,16 @@
-import {React,useState} from 'react'
+import { React, useState } from 'react'
 import { Link } from 'react-router-dom'
 import { AppBar, Box, Tab, Tabs, Toolbar, Typography } from "@mui/material"
 const Header = () => {
-    const [value, setvalue] = useState()
+    const [value, setvalue] = useState(0)
     return (
         <div>
             <AppBar position="sticky">
                 <Toolbar>
                     <Typography variant="h3">MernAuth</Typography>
-                    <Box sx={{marginLeft:"auto"}} >
+                    <Box sx={{ marginLeft: "auto" }} >
                         <Tabs indicatorColor='secondary'
-                        onChange={(e,val)=>setvalue(val)} value={value} textColor="inherit">
+                            onChange={(e, val) => setvalue(val)} value={value} textColor="inherit">
                             <Tab to='/Login' LinkComponent={Link} label="Login"></Tab>
                             <Tab to='/Signup' LinkComponent={Link} label="Signup"></Tab>
                         </Tabs>
--- frontend/src/components/Signup.js
+++ frontend/src/components/Signup.js
@@ -1,23 +1,37 @@
 import { Button, TextField, Typography } from "@mui/material";
 import { Box } from "@mui/system";
+import axios from "axios"
 import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const Signup = () => {
+  const history=useNavigate()
   const [inputs, setinputs] = useState({
-    name:"",
-    email:"",
-    password:""
-  }
-  )
-  const handleChange=(e)=>{
-    setinputs((prev)=>({
+    name: "",
+    email: "",
+    password: "",
+  });
+  const handleChange = (e) => {
+    setinputs((prev) => ({
       ...prev,
-      [e.target.name]:e.target.value,
-    }))
-  }
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  const sendRequest = async () => {
+    const res = axios.post('http://localhost:5000/api/signup',{
+      name:inputs.name,
+      email:inputs.email,
+      password:inputs.password
+    }).catch((err)=>console.log(err))
+    const data=await res.data
+    return data
+  };
+
   const handleSumbit = (e) => {
-    e.preventDefault()
-    console.log(inputs)
+    e.preventDefault();
+   //send http request
+   sendRequest().then(()=>history("/login"))
   };
 
   return (
@@ -33,10 +47,35 @@ const Signup = () => {
           alignItems="center"
         >
           <Typography variant="h2">SignUp</Typography>
-          <TextField name="name"  onChange={handleChange} value={inputs.name} variant="outlined" placeholder="Name" margin="normal" />
-          <TextField  name="email" onChange={handleChange} type={"email"} value={inputs.email} variant="outlined" placeholder="Email" margin="normal" />
-          <TextField  name="password" onChange={handleChange} type={"password"} value={inputs.password} variant="outlined" placeholder="Password" margin="normal" />
-          <Button variant="contained" type="sumbit">Signup</Button>
+          <TextField
+            name="name"
+            onChange={handleChange}
+            value={inputs.name}
+            variant="outlined"
+            placeholder="Name"
+            margin="normal"
+          />
+          <TextField
+            name="email"
+            onChange={handleChange}
+            type={"email"}
+            value={inputs.email}
+            variant="outlined"
+            placeholder="Email"
+            margin="normal"
+          />
+          <TextField
+            name="password"
+            onChange={handleChange}
+            type={"password"}
+            value={inputs.password}
+            variant="outlined"
+            placeholder="Password"
+            margin="normal"
+          />
+          <Button variant="contained" type="sumbit">
+            Signup
+          </Button>
         </Box>
       </form>
     </div>
