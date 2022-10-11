51a46866a7ad390f6e73e7fc632da577ca833419
Nikhil <nikhilsimontoppo@gmail.com>
2022-10-11 17:51:09 +0530

    fck

--- Backend/app.js
+++ Backend/app.js
@@ -10,6 +10,6 @@ app.use(cookieParser())
 app.use(express.json())
 app.use('/api', router)
 mongoose.connect(`mongodb+srv://Simon:${process.env.MONGODB_PASSWORD}@new-hope.6kamamm.mongodb.net/mern-auth?retryWrites=true&w=majority`).then(() => {
-    app.listen(5000);
+    app.listen(process.env.PORT || 5000);
     console.log("Database is connected and im listening to port 5000")
 }).catch((err) => console.log(err))
