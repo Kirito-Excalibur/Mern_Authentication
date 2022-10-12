d77d840266fb531e437ceab8569b6c0ffa07e4f2
Nikhil <nikhilsimontoppo@gmail.com>
2022-10-12 07:16:48 +0530

    few minor changes

--- Backend/app.js
+++ Backend/app.js
@@ -9,7 +9,13 @@ app.use(cors({ credentials: true, origin: "http://localhost:3000" }))
 app.use(cookieParser())
 app.use(express.json())
 app.use('/api', router)
-mongoose.connect(`mongodb+srv://Simon:${process.env.MONGODB_PASSWORD}@new-hope.6kamamm.mongodb.net/mern-auth?retryWrites=true&w=majority`).then(() => {
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+  })
+
+
+mongoose.connect(`mongodb+srv://Simon:VnRfti_qN6DUydF@new-hope.6kamamm.mongodb.net/mern-auth?retryWrites=true&w=majority`).then(() => {
     app.listen(process.env.PORT || 5000);
     console.log("Database is connected and im listening to port 5000")
 }).catch((err) => console.log(err))
